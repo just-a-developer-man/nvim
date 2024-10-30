84c527139ac3c341d6cb785535628e0bdb0593f4
just-a-developer-man <167205303+just-a-developer-man@users.noreply.github.com>
2024-10-30 11:42:50 +0300

    Added nvim config

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,11 @@
+return {
+  "tokyonight.nvim",
+  opts = {
+    transparent = true,
+    style = "night",
+    styles = {
+      sidebars = "transparent",
+      floats = "transparent",
+    },
+  },
+}
