f26603e3457ef78f585535c707ae8f9833199665
just-a-developer-man <167205303+just-a-developer-man@users.noreply.github.com>
2024-11-03 13:09:42 +0300

    Initial config

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,11 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- New tab
+keymap.set("n", "te", ":tabedit")
+keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/tokyonight.nvim",
+  opts = function()
+    return {
+      transparent = true,
+      style = "night",
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    }
+  end,
+}
